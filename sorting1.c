5afc8bd8e17542c3de8e7e8325c782d053787ff6
Michael-alexandrov <michael-alexandrov@yandex.ru>
2016-10-05 10:17:59 +0300

    Add files via upload
    Сортировка методом простой вставки для простоты на 20 элементов, при упорядоченных трех первых элементах.

---
+++ sorting1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+	int a[20], i, j, c, e, r;
+	for (i = 0; i < 20; i++)
+		scanf("%d,", &a[i]);
+	for (i = 3; i < 20; i++){
+		r = 1;
+		j = 0;
+		while ((r == 1) && (j < i)){
+			if (a[i] < a[j]) {
+				c = a[i];
+				for (e = i; e > j; e--){
+					a[e] = a[e - 1];
+				}
+				a[j] = c;
+				r = 2;
+			}
+			j++;
+		}
+	}
+		for (i = 0; i < 20; i++)
+			printf("%d ", a[i]);
+			return 0;
+}
